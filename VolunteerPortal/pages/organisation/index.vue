2adae51bd54bf66011c5991056d7019bab554b37
zhixuan07 <80529102+zhixuan07@users.noreply.github.com>
2024-10-01 13:11:00 +0800

    feat: Org Index

---
+++ VolunteerPortal/pages/organisation/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+  layout: 'org'
+});
+</script>
+
+<template>
+  <div>
+    Dashboard
+  </div>
+</template>
+
+<style scoped></style>
